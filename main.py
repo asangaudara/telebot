dede3e637048eb1ecb68a91774b9965493b5fa85
AsangaBro <70792747+asangaudara@users.noreply.github.com>
2021-02-04 02:47:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("1534183615:AAHSWVl5i2ccUEaoHUzZ69YoR-g-_KJhbSA")
 @bot.message_handler(commands=['start', 'help'])
 
 def send_welcome(message):
-                         bot.reply_to(message, "Hello! I'm Android Wedakarayo Test Bot. How can I help you?")
+                         bot.reply_to(message, "Hello")
 
 
 @bot.message_handler(commands=['test'])
