4a9c5e5fb2267250b6eb2a0879b297f20a52ab93
AsangaBro <70792747+asangaudara@users.noreply.github.com>
2021-03-27 20:12:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import telebot
 
 bot = telebot.TeleBot("1604055296:AAGZ_M52cLbaTh16CqaJrkw0VEZuwcCmFc0")
 
-@bot.message_handler(commands=['start', 'help'])
-
-def send_welcome(message):
-                         bot.reply_to(message, "Hello")
-
 
 @bot.message_handler(commands=['test'])
 def start_message(message):  
